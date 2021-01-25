65e13f5aa8e511d424df9d39453c3bad04ee3098
Tomasz Belech <tomaszbelech1@gmail.com>
2021-01-25 15:32:38 +0100

    input with api key created

--- photo-searcher/src/App.js
+++ photo-searcher/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  //SIkhfMxSrmtPJUAQKEL6FBL1P8diE2z8ekIK - BqNTJk
+  const fetchImages = () => {
+    fetch(
+      `https://api.unsplash.com/search/photos?client_id=SIkhfMxSrmtPJUAQKEL6FBL1P8diE2z8ekIK-BqNTJk&query=${value}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mydiv">
+        <span>Search</span>
+        <input
+          style={{ width: "60%" }}
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={() => fetchImages()}>send</button>
+      </div>
     </div>
   );
 }
